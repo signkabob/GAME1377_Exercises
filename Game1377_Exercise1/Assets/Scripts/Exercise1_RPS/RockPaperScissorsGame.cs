74e94a25db16c8ea07e3d7f52c74ad8a258d91a2
Kabo <signkabob@gmail.com>
2026-06-15 14:31:48 -0500

    Implemented Exercise 01 feedback
    - Removed string array
    - Replaced magic number
    - Simplified conditions

--- Game1377_Exercise1/Assets/Scripts/Exercise1_RPS/RockPaperScissorsGame.cs
+++ Game1377_Exercise1/Assets/Scripts/Exercise1_RPS/RockPaperScissorsGame.cs
@@ -18,35 +18,33 @@ public class RockPaperScissorsGame : MonoBehaviour
         paper,
         scissors,
         lizard,
-        spock
+        spock,
+        length
     }
-
-    private string[] choices = { "rock", "paper", "scissors", "lizard", "spock" };
 
     public void RockPaperScissors(int playerChoice)
     {
         // Generate a random integer from 1 to 5 for the computer choice 
-        int computerChoice = Random.Range(1,6);
+        int computerChoice = Random.Range(1,(int) Choice.length);
+        // Set the computer as the default winner unless it loses
+        bool isWinner = false;
 
         // Display the choices from the player and the computer
         Debug.Log("You chose: " + (Choice) playerChoice);
         Debug.Log("Computer chose: " + (Choice) computerChoice);
 
-        // A tie between the player and the computer
+        // A tie between the player and the computer before exiting
         if ((Choice) playerChoice == (Choice) computerChoice)
         {
             Debug.Log("It's a tie! Both chose " + (Choice) playerChoice);
+            return;
         }
         // Rock beats Scissors and Lizard
         else if ((Choice) playerChoice == Choice.rock)
         {
             if ((Choice) computerChoice == Choice.scissors || (Choice) computerChoice == Choice.lizard)
             {
-                Debug.Log("You win! " + (Choice) playerChoice + " beats " + (Choice) computerChoice);
-            }
-            else
-            {
-                Debug.Log("You lose! " + (Choice) computerChoice + " beats " + (Choice) playerChoice);
+                isWinner = true;
             }
         }
         // Scissors beats Paper and Lizard
@@ -54,11 +52,7 @@ public class RockPaperScissorsGame : MonoBehaviour
         {
             if ((Choice)computerChoice == Choice.paper || (Choice)computerChoice == Choice.lizard)
             {
-                Debug.Log("You win! " + (Choice)playerChoice + " beats " + (Choice)computerChoice);
-            }
-            else
-            {
-                Debug.Log("You lose! " + (Choice)computerChoice + " beats " + (Choice)playerChoice);
+                isWinner = true;
             }
         }
         // Paper beats Rock and Spock
@@ -66,11 +60,7 @@ public class RockPaperScissorsGame : MonoBehaviour
         {
             if ((Choice)computerChoice == Choice.rock || (Choice)computerChoice == Choice.spock)
             {
-                Debug.Log("You win! " + (Choice)playerChoice + " beats " + (Choice)computerChoice);
-            }
-            else
-            {
-                Debug.Log("You lose! " + (Choice)computerChoice + " beats " + (Choice)playerChoice);
+                isWinner = true;
             }
         }
         // Lizard beats Paper and Spock
@@ -78,11 +68,7 @@ public class RockPaperScissorsGame : MonoBehaviour
         {
             if ((Choice)computerChoice == Choice.paper || (Choice)computerChoice == Choice.spock)
             {
-                Debug.Log("You win! " + (Choice)playerChoice + " beats " + (Choice)computerChoice);
-            }
-            else
-            {
-                Debug.Log("You lose! " + (Choice)computerChoice + " beats " + (Choice)playerChoice);
+                isWinner = true;
             }
         }
         // Spock beats Scissors and Rock
@@ -90,12 +76,18 @@ public class RockPaperScissorsGame : MonoBehaviour
         {
             if ((Choice)computerChoice == Choice.scissors || (Choice)computerChoice == Choice.rock)
             {
-                Debug.Log("You win! " + (Choice)playerChoice + " beats " + (Choice)computerChoice);
+                isWinner = true;
             }
-            else
-            {
-                Debug.Log("You lose! " + (Choice)computerChoice + " beats " + (Choice)playerChoice);
-            }
+        }
+
+        // Display the result message
+        if (isWinner)
+        {
+            Debug.Log("You win! " + (Choice)playerChoice + " beats " + (Choice)computerChoice);
+        }
+        else
+        {
+            Debug.Log("You lose! " + (Choice)computerChoice + " beats " + (Choice)playerChoice);
         }
     }
 }
