3327c72bad6c05b50a0bc954e16dae56489a859d
Kabo <signkabob@gmail.com>
2026-06-15 14:42:48 -0500

    Proper naming for m-variables
    'm' is now 'monster'

--- Game1377_Exercise2/Assets/Scripts/Exercise2_MonsterBrawl/MonsterBrawl.cs
+++ Game1377_Exercise2/Assets/Scripts/Exercise2_MonsterBrawl/MonsterBrawl.cs
@@ -1,7 +1,7 @@
 /**
  * Exercise 02 - MonsterBrawl.cs
  * Name: Ka Bo Cheung
- * Date: 06/13/2026
+ * Date: 06/15/2026
  * Course: GAME-1377-001
  *
  *Script for the battle simulation between monsters
@@ -24,59 +24,59 @@ public class MonsterBrawl : MonoBehaviour
         }
 
         // Declare variables for the battle simulation
-        int m1Health;
-        int m2Health;
+        int monster1Health;
+        int monster2Health;
         int turn;
 
         // Monster #1 selection
-        for (int m1 = 0;  m1 < monsterNames.Length; m1++)
+        for (int monster1 = 0;  monster1 < monsterNames.Length; monster1++)
         {
             // Monster #2 selection; Skip used monster #1 for each iteration 
-            for (int m2 = m1; m2 < monsterNames.Length; m2++)
+            for (int monster2 = monster1; monster2 < monsterNames.Length; monster2++)
             {
                 // Monster shouldn't fight its own kind
-                if (m1 == m2)
+                if (monster1 == monster2)
                 {
                     continue;
                 } 
 
                 // Initialize variables for each unique battle
-                m1Health = healthStats[m1];
-                m2Health = healthStats[m2];
+                monster1Health = healthStats[monster1];
+                monster2Health = healthStats[monster2];
                 turn = 0;
 
                 // Battle simulation between monster #1 and monster #2 until either one is dead
-                while (m1Health > 0 && m2Health > 0)
+                while (monster1Health > 0 && monster2Health > 0)
                 {
                     turn++;
                     // Monster #1's turn based on its speed
-                    if (turn % speedStats[m1] == 0)
+                    if (turn % speedStats[monster1] == 0)
                     {
-                        m2Health -= attackStats[m1]; 
+                        monster2Health -= attackStats[monster1]; 
                     }
                     // Monster #2's turn based on its speed
-                    if (turn % speedStats[m2] == 0)
+                    if (turn % speedStats[monster2] == 0)
                     {
-                        m1Health -= attackStats[m2];
+                        monster1Health -= attackStats[monster2];
                     }
                 }
 
                 // If both monsters die on the same turn...
-                if (m1Health <= 0 && m2Health <= 0)
+                if (monster1Health <= 0 && monster2Health <= 0)
                 {
-                    Debug.Log(monsterNames[m1] + " vs " + monsterNames[m2] + " | Draw | Turns: " + turn);
+                    Debug.Log(monsterNames[monster1] + " vs " + monsterNames[monster2] + " | Draw | Turns: " + turn);
                 }
                 // If monster #1 defeats monster #2...
-                else if (m2Health <= 0)
+                else if (monster2Health <= 0)
                 {
-                    Debug.Log(monsterNames[m1] + " vs " + monsterNames[m2] + " | Winner: " + monsterNames[m1] 
-                        + " | Turns: " + turn + " | Remaining HP: " + m1Health);
+                    Debug.Log(monsterNames[monster1] + " vs " + monsterNames[monster2] + " | Winner: " + monsterNames[monster1] 
+                        + " | Turns: " + turn + " | Remaining HP: " + monster1Health);
                 }
                 // If monster #2 defeats monster #1...
                 else
                 {
-                    Debug.Log(monsterNames[m1] + " vs " + monsterNames[m2] + " | Winner: " + monsterNames[m2]
-                        + " | Turns: " + turn + " | Remaining HP: " + m2Health);
+                    Debug.Log(monsterNames[monster1] + " vs " + monsterNames[monster2] + " | Winner: " + monsterNames[monster2]
+                        + " | Turns: " + turn + " | Remaining HP: " + monster2Health);
                 }
             }
         }
